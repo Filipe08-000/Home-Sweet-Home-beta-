10b5636eba529e1b97a6792c8d70eb618ce35e11
Filipe <filipefg888@gmail.com>
2026-03-11 15:14:55 -0300

    Initialize database connection with pg and dotenv

---
+++ test-db.js
@@ -0,0 +1,11 @@
+require("dotenv").config()
+const { Pool } = require("pg")
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL
+})
+
+pool.query("SELECT NOW()", (err, res) => {
+  console.log(err, res.rows)
+  process.exit()
+})
